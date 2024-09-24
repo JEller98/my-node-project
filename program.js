2a6f50b5d9d0a5ae1c619b90bf59acd51d1b6233
jEllerGRC <eller.jared@student.greenriver.edu>
2024-09-24 13:54:58 -0700

    Added a loop.

--- program.js
+++ program.js
@@ -1,3 +1,7 @@
 console.log("Hello from Nodejs!");
 console.log("The date is September 23, 2024");
 console.log("My name is Jared! Thanks for stopping by!");
+
+for (let i = 1; i < 10; i++) {
+    console.log(i);
+}
